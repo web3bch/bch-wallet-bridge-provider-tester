df12daccc1e02ca868de8871c669bedabad8ce96
Akifumi Fujita <tessitetsusi@gmail.com>
2018-11-13 13:21:25 +0900

    add tester baasae page

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,52 @@ import "./App.css"
 
 const App: React.SFC = () => (
   <div>
-    <p>hoge</p>
+    <h1>BCH Wallet Bridge Provider Tester</h1>
+    <TestTable tests={TESTS}/>
   </div>
 )
+
+interface ITestProps {
+  name: string
+  result: boolean
+}
+
+const TestRow: React.SFC<ITestProps> = (props: ITestProps) => (
+  <tr>
+    <td><button>{props.name}</button></td>
+    <td>{props.result}</td>
+  </tr>
+)
+
+interface ITestsProps {
+  tests: ITestProps[]
+}
+
+const TestTable: React.SFC<ITestsProps> = (props: ITestsProps) => (
+  <div>
+    <button onClick={onTestButtonClicked()}>Start Tests</button>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Result</th>
+        </tr>
+      </thead>
+      <tbody>{props.tests.map(renderRow)}</tbody>
+    </table>
+  </div>
+)
+
+const renderRow = (test: ITestProps, idx: number) => (
+  <TestRow key={idx} name={test.name} result={test.result} />
+)
+
+let TESTS = [
+  {name: "test 1", result: false}
+]
+
+const onTestButtonClicked = () => () => {
+  TESTS = [ {name: "test 2", result: false} ]
+}
 
 export default App
