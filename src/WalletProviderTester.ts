d00be19a8d7dbd38f8a9e51b74ed227c67b3859a
Shun Usami <usatie@gmail.com>
2018-11-13 13:24:08 +0900

    :sparkles: Add sample test

--- src/WalletProviderTester.ts
+++ src/WalletProviderTester.ts
@@ -2,4 +2,16 @@ import IWalletProvider from "bch-wallet-bridge-provider-interface/lib/IWalletPro
 
 class WalletProviderTester {
     constructor(public walletProvider: IWalletProvider) {}
+
+    public testGetAddresses(): Promise<boolean> {
+        return this.walletProvider
+        .getAddresses(0, 10, 0, undefined)
+        .then((addresses) => {
+            const address = addresses[0]
+            if (typeof address !== "string") {
+              throw new Error("The return value is invalid.")
+            }
+            return addresses.length === 10
+          })
+    }
 }
